7d62b0436adb2391a7c04cff013bb259d35b7b3b
Suchithra <suchitra.nikki@gmail.com>
2023-06-28 16:02:11 +1000

    Add CRUD operations

--- routes/dress/index.js
+++ routes/dress/index.js
@@ -1,43 +1,76 @@
 const express = require('express')
-
 const router = express.Router()
-
 const db = require('../../db')
 
+// ROUTES
+
+// Get all dresses posted by the logged in user
 router.get('/', (req, res) => {
-  const dressId = req.params.id
   const sql = `SELECT * FROM dress;`
   db.query(sql, (err, dbRes) => {
     console.log(err, dbRes.rows[0])
-    // res.send('item retrieval')
-    res.render('dresses/show', { dress: dbRes.rows[0] })
+    res.render('dresses/index', { dresses: dbRes.rows })
   })
 })
 
+// Get the post form to add new post
 router.get('/new', (req, res) => {
   res.render('dresses/new')
 })
 
+// Get a specific dress by id posted by the logged in user
 router.get('/:id', (req, res) => {
   const dressId = req.params.id
   const sql = `SELECT * FROM dress WHERE id=$1`
   db.query(sql, [dressId], (err, dbRes) => {
     console.log(err, dbRes.rows)
-    // res.send('item retrieval')
     res.render('dresses/show', { dress: dbRes.rows[0] })
   })
 })
 
+// Create a post
 router.post('/', (req, res) => {
-  const sql = `INSERT INTO dress (title, price, photo_url, posted_by) VALUES ($1, $2, $3, 1) RETURNING id;`
-
   const title = req.body.title
   const price = req.body.price
   const photUrl = req.body.imageurl
-
+  const sql = `INSERT INTO dress (title, price, photo_url, posted_by) VALUES ($1, $2, $3, 1) RETURNING id;`
   db.query(sql, [title, price, photUrl], (err, dbRes) => {
     console.log(err)
     const dressId = dbRes.rows[0].id
+    res.redirect(`/dresses/${dressId}`)
+  })
+})
+
+// Delete a post
+router.delete('/:id', (req, res) => {
+  const dressId = req.params.id
+  const sql = `DELETE FROM dress WHERE id=$1`
+
+  db.query(sql, [dressId], (err, dbRes) => {
+    res.redirect('/dresses')
+  })
+})
+
+// Get the edit form to modify a new post
+router.get('/:id/edit', (req, res) => {
+  const dressId = req.params.id
+  const sql = `SELECT * FROM dress WHERE id=$1`
+  db.query(sql, [dressId], (err, dbRes) => {
+    console.log(err, dbRes.rows)
+    res.render('dresses/edit', { dress: dbRes.rows[0] })
+  })
+  // res.render('dresses/edit')
+})
+
+// Modify a post
+router.put('/:id', (req, res) => {
+  const dressId = req.params.id
+  const title = req.body.title
+  const price = req.body.price
+  const photUrl = req.body.imageurl
+  const sql = `UPDATE dress  SET title=$1 , price=$2, photo_url=$3, posted_by=$4 WHERE id=$5;`
+  db.query(sql, [title, price, photUrl, 1, dressId], (err, dbRes) => {
+    console.log(err, dbRes)
     res.redirect(`/dresses/${dressId}`)
   })
 })
--- server.js
+++ server.js
@@ -1,23 +1,45 @@
+// Load ENV config file
 require('dotenv').config()
+
+// Imports
 const express = require('express')
 const app = express()
+const dressRouter = require('./routes/dress')
+const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+
+// App setting
+app.set('view engine', 'ejs')
+
+// Global variables
 const port = process.env.PORT || 5000
 
-const dressRouter = require('./routes/dress')
-
-app.set('view engine', 'ejs')
-const expressLayouts = require('express-ejs-layouts')
-
+// MIDDLEWARES
+// To fetch the post requrest body
 app.use(express.urlencoded({ extended: true }))
-
+// To fetch the static files
 app.use(express.static('public'))
+// To use express layouts
 app.use(expressLayouts)
+// To accept put and delete request as post
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST body and delete it
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
+// Routers
 app.get('/', (req, res) => {
   res.render('home')
 })
-
 app.use('/dresses', dressRouter)
 
+// Server request Listener
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
